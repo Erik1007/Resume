7a157acd24368339afcd882a5231f47efa5eb1e8
Erik1007 <erik.hartman1007@gmail.com>
2022-10-21 12:08:08 +0000

    added script and error response to github page with loading gif

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -6,16 +6,28 @@
 
             var labels = "ABCDEFGHIJKLMNOPQRSTUV";
 
-            var locations = [
-                {lat: 68.4301, lng: 18.1254},
-                {lat: 60.0971, lng: 19.9348},
-                {lat: 45.9238, lng: 6.8693}
+            var locations = [{
+                    lat: 68.4301,
+                    lng: 18.1254
+                },
+                {
+                    lat: 60.0971,
+                    lng: 19.9348
+                },
+                {
+                    lat: 45.9238,
+                    lng: 6.8693
+                }
             ];
 
-            var markers = location.map(function(location, i) {
+            var markers = location.map(function (location, i) {
                 return new google.maps.Marker({
-                    position: location, 
+                    position: location,
                     label: labels[i % labels.length]
                 });
+            });
+
+            var markerCluster = new MarkerClusterer(map, markers, {
+                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
             });
         }
