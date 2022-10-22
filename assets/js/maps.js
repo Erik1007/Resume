b96d402ee45ae0bd802b063427f187eeaacd7f16
Erik1007 <erik.hartman1007@gmail.com>
2022-10-22 13:26:02 +0000

    added to the github fetch presenting user information in the page

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,33 +1,34 @@
-        function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                center: new google.maps.LatLng(59.334591, 18.063240),
-                zoom: 5,
-            });
-
-            var labels = "ABCDEFGHIJKLMNOPQRSTUV";
-
-            var locations = [{
-                    lat: 68.4301,
-                    lng: 18.1254
-                },
-                {
-                    lat: 60.0971,
-                    lng: 19.9348
-                },
-                {
-                    lat: 45.9238,
-                    lng: 6.8693
-                }
-            ];
-
-            var markers = location.map(function (location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
-
-            var markerCluster = new MarkerClusterer(map, markers, {
-                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-            });
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
         }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [{
+        lat: 68.4301,
+        lng: 18.1254
+    },
+    {
+        lat: 60.0971,
+        lng: 19.9348
+    },
+    {
+        lat: 45.9238,
+        lng: 6.8693
+    }
+];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
